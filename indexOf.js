ef05b5ad5dea70514ffbc982e3decfae2d93c523
Johan Lidberg <johan.lidberg@skola.botkyrka.se>
2020-11-23 15:33:51 +0100

    steg 1

--- indexOf.js
+++ indexOf.js
@@ -1,7 +1,15 @@
 
 function indexOf(arr, item) {
-    
+    for (let i = 0; i < array.length; i++) {
+      if (arr[i] === item) {
+          return true
+      }
+        
+    }
+    return false
 }
+
+
 
 const items = [
     'Bicycle',
@@ -17,9 +25,5 @@ console.log(indexOf(items, 'Teddybear'))    // Should be 3
 console.log(indexOf(items, 'Laser sword'))  // Should be -1
 console.log(indexOf(items, 'Playstation'))  // Should be -1
 
-// ------------------
 
-console.log(items.indexOf('Lego'))         // Should be 1
-console.log(items.indexOf('Teddybear'))    // Should be 3
-console.log(items.indexOf('Laser sword'))  // Should be -1
-console.log(items.indexOf('Playstation'))  // Should be -1
+
--- statistics.js
+++ statistics.js
@@ -1,7 +1,13 @@
 
 function max(arr) {
-
+for (let i = 0; i < array.length; i++) {
+   if (arr[i] >= max){
+max = arr[i];
+   }
+    
 }
+}
+console.log(max)
 
 function min(arr) {
 
